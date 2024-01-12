51f0ba26da18fd3ffbc9f76e19544027d02b54b1
chumyshhh[llI] <vandarkslave@outlook.com>
2024-01-12 18:17:41 +0300

    changed return type, added inline fun

--- app/src/main/java/com/internship/data/remote/ApiService.kt
+++ app/src/main/java/com/internship/data/remote/ApiService.kt
@@ -2,9 +2,10 @@ package com.internship.data.remote
 
 import com.internship.data.remote.dto.CamerasResponseDto
 import com.internship.data.remote.dto.DoorResponseDto
+import com.internship.utils.Resource
 
 interface ApiService {
 
-    suspend fun getCams(): CamerasResponseDto?
-    suspend fun getDoors(): DoorResponseDto?
+    suspend fun getCams(): Resource<CamerasResponseDto>
+    suspend fun getDoors(): Resource<DoorResponseDto>
 }
--- app/src/main/java/com/internship/data/remote/ApiServiceImpl.kt
+++ app/src/main/java/com/internship/data/remote/ApiServiceImpl.kt
@@ -1,35 +1,54 @@
 package com.internship.data.remote
 
+import android.util.Log
 import com.internship.data.remote.dto.CamerasResponseDto
 import com.internship.data.remote.dto.DoorResponseDto
+import com.internship.utils.Resource
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
+import io.ktor.client.plugins.ClientRequestException
+import io.ktor.client.plugins.RedirectResponseException
+import io.ktor.client.plugins.ServerResponseException
 import io.ktor.client.request.get
 import io.ktor.client.request.url
+import io.ktor.client.statement.HttpResponse
 import javax.inject.Inject
 
 class ApiServiceImpl @Inject constructor(
     private val client: HttpClient
 ) : ApiService {
-    override suspend fun getCams(): CamerasResponseDto? {
-        return try {
+    override suspend fun getCams(): Resource<CamerasResponseDto> {
+        return makeRequest<CamerasResponseDto> {
             client.get {
                 url(HttpRoutes.CAMERAS)
-            }.body()
-        } catch (e: Exception) {
-            println(e.message)
-            null
+            }
         }
     }
 
-    override suspend fun getDoors(): DoorResponseDto? {
-        return try {
+    override suspend fun getDoors(): Resource<DoorResponseDto> {
+        return makeRequest<DoorResponseDto> {
             client.get {
                 url(HttpRoutes.DOORS)
-            }.body()
+            }
+        }
+    }
+
+    private suspend inline fun <reified T> makeRequest(block: () -> HttpResponse): Resource<T> {
+        return try {
+            val result = block().body<T>()
+            Resource.Success(data = result)
+        } catch (e: RedirectResponseException) {
+            Log.e("RedirectResponseException", e.message)
+            Resource.Error(message = e.message)
+        } catch (e: ClientRequestException) {
+            Log.e("ClientRequestException", e.message)
+            Resource.Error(message = e.message)
+        } catch (e: ServerResponseException) {
+            Log.e("ServerResponseException", e.message)
+            Resource.Error(message = e.message)
         } catch (e: Exception) {
-            println(e.message)
-            null
+            Log.e("Exception", e.message ?: "")
+            Resource.Error(message = e.message ?: "")
         }
     }
 }
