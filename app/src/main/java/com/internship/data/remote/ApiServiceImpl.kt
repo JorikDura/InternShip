f5f25fde0ff0f188dd9cf35eb270e039589d103a
chumyshhh[llI] <vandarkslave@outlook.com>
2024-01-12 19:10:06 +0300

    added NoTransformationFoundException

--- app/src/main/java/com/internship/data/remote/ApiServiceImpl.kt
+++ app/src/main/java/com/internship/data/remote/ApiServiceImpl.kt
@@ -5,6 +5,7 @@ import com.internship.data.remote.dto.CamerasResponseDto
 import com.internship.data.remote.dto.DoorResponseDto
 import com.internship.utils.Resource
 import io.ktor.client.HttpClient
+import io.ktor.client.call.NoTransformationFoundException
 import io.ktor.client.call.body
 import io.ktor.client.plugins.ClientRequestException
 import io.ktor.client.plugins.RedirectResponseException
@@ -39,13 +40,16 @@ class ApiServiceImpl @Inject constructor(
             Resource.Success(data = result)
         } catch (e: RedirectResponseException) {
             Log.e("RedirectResponseException", e.message)
-            Resource.Error(message = e.message)
+            Resource.Error(message = e.response.status.description)
         } catch (e: ClientRequestException) {
             Log.e("ClientRequestException", e.message)
-            Resource.Error(message = e.message)
+            Resource.Error(message = e.response.status.description)
         } catch (e: ServerResponseException) {
             Log.e("ServerResponseException", e.message)
-            Resource.Error(message = e.message)
+            Resource.Error(message = e.response.status.description)
+        } catch (e: NoTransformationFoundException) {
+            Log.e("NoTransformationFoundException", e.message ?: "")
+            Resource.Error(message = e.message ?: "")
         } catch (e: Exception) {
             Log.e("Exception", e.message ?: "")
             Resource.Error(message = e.message ?: "")
